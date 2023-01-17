9207a099dd0f402e3d24fd0101a8707f8aa0db79
Sorethea OK <sorethea@gmail.com>
2023-01-17 15:03:26 +0700

    lam-dev

--- Filament/Resources/RoleResource.php
+++ Filament/Resources/RoleResource.php
@@ -17,7 +17,7 @@ class RoleResource extends Resource
 {
     protected static ?string $model = Role::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-lock';
+    protected static ?string $navigationIcon = 'heroicon-o-key';
 
     protected static function getNavigationGroup(): ?string
     {
